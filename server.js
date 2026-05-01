6054188292a36bbee6d9378f0ebcffcedabcbe1a
TONG VAN DUNG <tongvandung@gmail.com>
2026-05-01 17:16:11 +0700

    Cập nhật hệ thống Fullstack và Socket.io

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('./')); // Phục vụ file giao diện tĩnh
+app.use(express.static(path.join(__dirname, './'))); // Phục vụ toàn bộ thư mục gốc
+
+// Mặc định vào trang index.html khi truy cập link
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const server = http.createServer(app);
 const io = new Server(server, {
